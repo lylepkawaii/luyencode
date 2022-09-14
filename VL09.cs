6d1cad7550314daea7dc7cbe3de9c4af7f3f8407
lylepkawaii <90955430+lylepkawaii@users.noreply.github.com>
2022-09-14 15:16:21 +0700

    Update VL09.cs

--- VL09.cs
+++ VL09.cs
@@ -1,22 +1,22 @@
 using System;
-
-namespace vl09
+namespace vl09_nguyendieuly
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-            var s1 = s.Split(' ');
-            int a = Convert.ToInt32(s1[0]);
-            int b = Convert.ToInt32(s1[1]);
-            a = Math.Abs(a);
-            b = Math.Abs(b);
-            for(int i=1; i<=a*b; i++)
-                if ((i % a == 0) && (i % b == 0)){ 
-                Console.WriteLine(i);
-                break;
+            string s1 = Console.ReadLine();
+            var s2 = s1.Split(' ');
+            double x = Convert.ToDouble(s2[0]);
+            int n = Convert.ToInt32(s2[1]);
+            double s = 0;
+            double bieuthuc = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                bieuthuc = bieuthuc * x / i;
+                s = s + bieuthuc;
             }
+            Console.WriteLine(decimal.Parse(s.ToString("0.00")));
         }
     }
 }
